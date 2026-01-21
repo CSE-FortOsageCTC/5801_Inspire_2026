c5b0bd5dc9de848d7eb873c770c34b04d7fc78c6
Noahg3291 <noahgschool24@gmail.com>
2026-01-21 15:40:18 -0600

    tried to fix speed, extention encoder, and other issues

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -1,17 +1,44 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import com.ctre.phoenix6.controls.Follower;
 
 
 public class ClimbSubsystem {
-    public static TalonFX leftClimber;
-    public static TalonFX rightClimber;
+    private static TalonFX MasterClimb;
+    private static TalonFX FollowerClimb;
+    private static ClimbSubsystem climbSubsystem;
+    private static ProfiledPIDController pidController;
+    //private static ArmPosition lastExtensionPosition = ArmPosition.Travel;
+    
 
-    public ClimbSubsystem(){
+    public static ClimbSubsystem getInstance(){
+        if (climbSubsystem == null){
+            climbSubsystem = new ClimbSubsystem();
+        } 
+        return (climbSubsystem);
+    }
+
+
+    private ClimbSubsystem(){
         // s_Swerve = Swerve.getInstance();
         // ledSubsystem = LEDSubsystem.getInstance();
-        
-        leftClimber = new TalonFX(0);
-        rightClimber = new TalonFX(0);
+
+        MasterClimb = new TalonFX(0);
+        FollowerClimb = new TalonFX(0);
+
+
+        FollowerClimb.setControl(new Follower(MasterClimb.getDeviceID(), MotorAlignmentValue.Aligned));
   }
+  public double getExtensionEncoder() {
+        return MasterClimb.getPosition().getValueAsDouble();
+    }
+    public void setClimbSpeed(double speed){
+        MasterClimb.set(speed);
+    
+}
 }
