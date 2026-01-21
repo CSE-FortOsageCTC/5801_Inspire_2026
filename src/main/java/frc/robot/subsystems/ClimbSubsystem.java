a7e377983de85a20a1212c532ef9fb9563d3d38b
Noahg3291 <noahgschool24@gmail.com>
2026-01-21 17:30:01 -0600

    made requested changes

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -3,44 +3,39 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix6.controls.Follower;
-
 
 public class ClimbSubsystem extends SubsystemBase {
     private static TalonFX MasterClimb;
     private static TalonFX FollowerClimb;
     private static ClimbSubsystem climbSubsystem;
     private static ProfiledPIDController pidController;
-    //private static ArmPosition lastExtensionPosition = ArmPosition.Travel;
-    
 
-    public static ClimbSubsystem getInstance(){
-        if (climbSubsystem == null){
+
+    public static ClimbSubsystem getInstance() {
+        if (climbSubsystem == null) {
             climbSubsystem = new ClimbSubsystem();
-        } 
+        }
         return (climbSubsystem);
     }
 
-
-    private ClimbSubsystem(){
-        // s_Swerve = Swerve.getInstance();
-        // ledSubsystem = LEDSubsystem.getInstance();
+    private ClimbSubsystem() {
 
         MasterClimb = new TalonFX(0);
         FollowerClimb = new TalonFX(0);
 
-
         FollowerClimb.setControl(new Follower(MasterClimb.getDeviceID(), MotorAlignmentValue.Aligned));
-  }
+    }
+
     public double getExtensionEncoder() {
         return MasterClimb.getPosition().getValueAsDouble();
     }
-    public void setClimbSpeed(double speed){
+
+    public void setClimbSpeed(double speed) {
         MasterClimb.set(speed);
-    
-}
+
+    }
 }
