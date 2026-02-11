5f9ffcd2e40801be47460c6d1c2636af0b1f0aa1
haz3rdP <borkdoggo01@gmail.com>
2026-02-11 16:19:13 -0600

    added getAutonomousCommand

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -148,7 +148,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return new Command() {};
+    return autoChooser.selectedCommand();
   }
 }
