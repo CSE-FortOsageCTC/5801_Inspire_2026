8125319c8916afa9763e21a013bbff8f0cecf6e8
Noahg3291 <noahgschool24@gmail.com>
2026-03-04 16:34:22 -0600

    sorry

--- src/main/java/frc/robot/commands/ShooterDefault.java
+++ src/main/java/frc/robot/commands/ShooterDefault.java
@@ -84,15 +84,11 @@ public class ShooterDefault extends Command {
         }
     }
 
-<<<<<<< turret_preemtive_turn
+
     private void attemptToShoot(double motorSpeed){
         if (ShooterSubsystem.getIsShooting()) {
             s_ShooterSubsystem.setFlywheels(motorSpeed);
-=======
-    private void attemptToShoot(){
-        if (ShooterSubsystem.getIsShooting()) {
-            s_ShooterSubsystem.setFlywheels(1);
->>>>>>> main
+
             if (delayCounter >= 25){ //0.5 second delay
                 s_ShooterSubsystem.setKicker(1);
                 if (delayCounter >= 50){ //another 0.5 sec delay
@@ -120,11 +116,8 @@ public class ShooterDefault extends Command {
             double manualHood = operator.getRawAxis(XboxController.Axis.kRightY.value);
             s_ShooterSubsystem.setSwivelSetpoint(s_ShooterSubsystem.getSwivelSetpoint() + manualSwivel);
             // s_ShooterSubsystem.setHoodSetpoint(s_ShooterSubsystem.getHoodSetpoint() + manualHood); 
-<<<<<<< turret_preemtive_turn
+
             //  attemptToShoot(1);
-=======
-            // s_ShooterSubsystem.attemptToShoot();
->>>>>>> main
             return;
         }
 
@@ -203,11 +196,7 @@ public class ShooterDefault extends Command {
 
 
         // if (s_ShooterSubsystem.isSwivelReadyToShoot() && s_ShooterSubsystem.isHoodReadyToShoot()) { 
-<<<<<<< turret_preemtive_turn
             // attemptToShoot(motorSpeed);
-=======
-        //     s_ShooterSubsystem.attemptToShoot();
->>>>>>> main
 
     }
 
