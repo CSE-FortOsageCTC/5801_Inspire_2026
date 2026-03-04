d702e89c6beab040291c959ea95b45dd9ec33f13
Noahg3291 <noahgschool24@gmail.com>
2026-03-04 16:21:15 -0600

    fixed merge conflicts and corrected jesse's code

--- src/main/java/frc/robot/commands/ShooterDefault.java
+++ src/main/java/frc/robot/commands/ShooterDefault.java
@@ -30,6 +30,7 @@ public class ShooterDefault extends Command {
     private Joystick operator;
     private Pose3d targetPose;
 
+    private int delayCounter = 0;
 
 
     private boolean isManual = false;
@@ -83,6 +84,24 @@ public class ShooterDefault extends Command {
         }
     }
 
+    private void attemptToShoot(double motorSpeed){
+        if (ShooterSubsystem.getIsShooting()) {
+            s_ShooterSubsystem.setFlywheels(motorSpeed);
+            if (delayCounter >= 25){ //0.5 second delay
+                s_ShooterSubsystem.setKicker(1);
+                if (delayCounter >= 50){ //another 0.5 sec delay
+                    s_ShooterSubsystem.setSpindexer(0.1);
+                }
+            }
+            delayCounter++;
+        } else {
+            s_ShooterSubsystem.setFlywheels(0);
+            s_ShooterSubsystem.setKicker(0);
+            s_ShooterSubsystem.setSpindexer(0);
+            delayCounter = 0;
+        }
+    }
+
     @Override
     public void execute() {
 
@@ -94,14 +113,8 @@ public class ShooterDefault extends Command {
             double manualSwivel = 5 * MathUtil.applyDeadband(operator.getRawAxis(XboxController.Axis.kRightX.value), Constants.stickDeadband);
             double manualHood = operator.getRawAxis(XboxController.Axis.kRightY.value);
             s_ShooterSubsystem.setSwivelSetpoint(s_ShooterSubsystem.getSwivelSetpoint() + manualSwivel);
-            // s_ShooterSubsystem.setHoodSetpoint(s_ShooterSubsystem.getHoodSetpoint() + manualHood);
-            // if (ShooterSubsystem.getIsShooting()) { 
-            //     s_ShooterSubsystem.setFlywheels(1);
-            //     s_ShooterSubsystem.setKicker(1);
-            // } else {
-            //     s_ShooterSubsystem.setFlywheels(0);
-            //     s_ShooterSubsystem.setKicker(0);
-            // }
+            // s_ShooterSubsystem.setHoodSetpoint(s_ShooterSubsystem.getHoodSetpoint() + manualHood); 
+            //  attemptToShoot(1);
             return;
         }
 
@@ -163,6 +176,7 @@ public class ShooterDefault extends Command {
         if (robotRelativeSwivelEncoder <= Constants.maximumSwivelEncoder && robotRelativeSwivelEncoder >= Constants.minimumSwivelEncoder) {
             s_ShooterSubsystem.setSwivelSetpoint(robotRelativeSwivelEncoder);
 
+
         }
         else if (thetaDegrees <= -90 && thetaDegrees >= -120) {
             s_ShooterSubsystem.setSwivelSetpoint(0);
@@ -170,22 +184,16 @@ public class ShooterDefault extends Command {
         else if (thetaDegrees >= 90 && thetaDegrees <= 120){
             s_ShooterSubsystem.setSwivelSetpoint(Constants.maximumSwivelEncoder);
         }
+
+
         
         // s_ShooterSubsystem.setSwivelSetpoint(robotRelativeSwivelEncoder);
 
         // s_ShooterSubsystem.setHoodSetpoint(launchAngleDegrees);
 
 
-    //     if (ShooterSubsystem.getIsShooting() && s_ShooterSubsystem.isSwivelReadyToShoot() && s_ShooterSubsystem.isHoodReadyToShoot()) { 
-    //         s_ShooterSubsystem.setFlywheels(motorSpeed);
-    //     } else {
-    //         s_ShooterSubsystem.setFlywheels(0);
-    //     }
-        
-
-
-
-        }
+        // if (s_ShooterSubsystem.isSwivelReadyToShoot() && s_ShooterSubsystem.isHoodReadyToShoot()) { 
+            // attemptToShoot(motorSpeed);
 
     }
 
