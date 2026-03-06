ff2021a065749ff2de4eb2e8d29babc5998d2c95
jk78543 <jkavanaug4112@student.fortosage.net>
2026-03-06 17:39:27 -0600

    changed logic to prevent shooting while under the trench

--- src/main/java/frc/robot/commands/ShooterDefault.java
+++ src/main/java/frc/robot/commands/ShooterDefault.java
@@ -32,6 +32,7 @@ public class ShooterDefault extends Command {
 
     private int delayCounter = 0;
 
+    private boolean isUnderTrench = false;
 
     private boolean isManual = false;
 
@@ -202,13 +203,14 @@ public class ShooterDefault extends Command {
 
         if (((turretPoseFieldRelative.getX() >= Constants.redTrenchAreaLeftX && turretPoseFieldRelative.getX() <= Constants.redTrenchAreaRightX) || (turretPoseFieldRelative.getX() >= Constants.blueTrenchAreaLeftX && turretPoseFieldRelative.getX() <= Constants.blueTrenchAreaRightX)) && (turretPoseFieldRelative.getY() >= Constants.TrenchAreaTopY || turretPoseFieldRelative.getY() <= Constants.TrenchAreaBottomY)) {
             s_ShooterSubsystem.setHoodSetpoint(Constants.minimumHoodEncoder);
-            return;
+            isUnderTrench = true;
         } 
         else {
             s_ShooterSubsystem.setHoodSetpoint(launchAngleDegrees / Constants.hoodEncoderPerDegree);
+            isUnderTrench = false;
         }
 
-        // if (s_ShooterSubsystem.isSwivelReadyToShoot() && s_ShooterSubsystem.isHoodReadyToShoot()) { 
+        // if (s_ShooterSubsystem.isSwivelReadyToShoot() && s_ShooterSubsystem.isHoodReadyToShoot() && !isUnderTrench) { 
             // attemptToShoot(motorSpeed);
 
     }
