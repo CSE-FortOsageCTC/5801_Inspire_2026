271f69f492e9278631f46b4647ae644c2bbf940b
T-Wezzy <t.wezzy087@gmail.com>
2026-03-07 14:52:57 -0600

    Added Field Relative Pose to Turret State

--- src/main/java/frc/robot/TurretState.java
+++ src/main/java/frc/robot/TurretState.java
@@ -1,15 +1,19 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+
 public class TurretState {
     
     public double turretDegrees;
     public double hoodDegrees;
     public double initialVelocity;
+    public Pose2d fieldRelativeFieldPose;
 
-    public TurretState(double turretDegrees, double hoodDegrees, double initialVelocity){
+    public TurretState(double turretDegrees, double hoodDegrees, double initialVelocity, Pose2d fieldRelativeFieldPose){
         this.turretDegrees = turretDegrees;
         this.hoodDegrees = hoodDegrees;
         this.initialVelocity = initialVelocity;
+        this.fieldRelativeFieldPose = fieldRelativeFieldPose;
     }
 
 }
